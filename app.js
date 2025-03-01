e24b6f5b97e5046bbad26e1f0beca86f5098eb3c
JavierPena5526 <javape5526@gmail.com>
2025-02-28 23:53:03 -0600

    added array and a object

---
+++ app.js
@@ -0,0 +1,22 @@
+const user = { 
+    age: 60,
+    ownHouse: true,
+    purchases: ["phone", "computer", "TV"],
+
+
+
+sayPurchases: function() {
+    console.log(this.purchases);
+    }    
+};
+
+user.sayPurchases();
+
+const list = ["Gym", "Groceries", "School",];
+
+list.push("Chores")
+
+console.log(list);
+
+
+
